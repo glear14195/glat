0d90fa285cf418a40f5b8ebf6fc42e241c187220
Akshat <akshat.sharma@okutech.in>
2017-01-27 17:22:27 +0530

    Added solrClient

--- try1.js
+++ try1.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var solr = require('solr-client');
+var client = solr.createClient({core:'gpsShit'});
 
+function gpsStore(req,res,next)
+{
+        client.add({name_s:req.body.name, location_p: req.body.lat+","+req.body.long  },function(err,obj){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Solr response:', obj);
+            client.commit(function(err,res){
+                if(err) console.log(err);
+                if(res) console.log(res);
+            });
+            next();
+        }
+        });
+}
 
 function getAll(req,res,next){
         var chunks = [];
@@ -19,12 +36,12 @@ function getAll(req,res,next){
             res.status(500);    
         });
 }
-/*
+
 app.use(function(req,res,next){
-                    console.log("FIFIFI");
+                    console.log(req.method+" request from "+req.ip);
                     next();
 });
-*/
+
 app.use(bodyParser.json());
 app.all('/swap',function(req,res){
     console.log(req.ip+" "+req.body.name);
@@ -33,6 +50,11 @@ app.all('/swap',function(req,res){
 app.all('/hello',function(req,res){
     console.log(req.ip+" "+req.body.name);
     res.send(JSON.stringify({"Msg":"Hello "+req.body.name,"Square":Math.pow(req.body.age,2)}));
+});
+
+app.all('/gps',gpsStore,function(req,res){
+    console.log("Probably Done?");
+    res.send("Done...");
 });
 
 app.listen('8080','0.0.0.0');
