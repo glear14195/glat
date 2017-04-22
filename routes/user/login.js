5d24be8d46501a884424f91ed5c4cb8af8e6052e
Akshat Sharma <akshat.sharma@fastfox.com>
2017-04-22 15:53:35 +0530

    adadd

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,48 +1,40 @@
 var pgclient = require('../../models/pgclient');
+var userHandler = require('../../lib/user');
 var cache = require('../../lib/cache');
 var chance = require('chance');
-var login =
-  function (req, res) {
-    var resp = { 'status': 'fail', 'err': '', 'resp': {} };
-    if (req && req.data.phone) {
-      pgclient.execute(`select count(*) from users where phone = '${req.data.phone}'`,
-        function (err, result) {
-          if (err) {
-            resp.err = err;
-            res.json(resp);
-          } else {
-            if (result.length && result[0].count !== '0') {
-              pgclient.execute(
-                `update users set is_verified = true where phone = '${req.data.phone}'`,
-                function (err, result) {
-                  if (err) {
-                    resp.err = err;
-                    res.json(resp);
-                  } else {
-                    cache.createToken(req.data.phone,
-                      function (err, token) {
-                        if (err) {
-                          resp.err = err;
-                          res.json(resp);
-                        } else {
-                          resp.status = 'success';
-                          resp.resp = { 'token': token };
-                          res.json(resp);
-                        }
-                      });
-                  }
-                }
-              );
+
+var login = function (req, res) {
+  var resp = { 'status': 'fail', 'err': '', 'resp': {} };
+  var phone = req.data.phone || ``;
+
+  if (req && phone) {
+    userHandler.markVerifiedUser(phone, function (err, result) {
+      if (err) {
+        resp.err = err;
+        res.json(resp);
+      } else {
+        if (result.length) {
+          cache.createToken(phone, function (err, token) {
+            if (err) {
+              console.log(`[ERR user/login] cache Error: ${err}`);
+              resp.err = err;
             } else {
-              resp.err = 'no such user';
-              res.json(resp);
+              resp.status = 'success';
+              resp.resp = { 'token': token };
             }
-          }
+            res.json(resp);
+          });
+        } else {
+          console.log(`[ERR user/login] no such user: ${phone}`);
+          resp.err = 'no such user';
+          res.json(resp);
         }
-      );
-    } else {
-      resp.err = 'Missing arguments'
-      res.json(resp);
-    }
+      }
+    });
+  } else {
+    console.log(`[ERR user/login] missing arguments: ${phone}`);
+    resp.err = 'Missing arguments';
+    res.json(resp);
   }
+}
 module.exports = login;
