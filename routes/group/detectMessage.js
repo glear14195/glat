af0fc036efee30f4d1fe06db31c6b5331a3cfadb
Akshat Sharma <akshat.sharma@fastfox.com>
2017-05-08 00:48:11 +0530

    Added base64 decode

--- routes/group/detectMessage.js
+++ routes/group/detectMessage.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var async = require('async');
 var exec = require('child_process').exec;
+var base64 = require('urlsafe-base64');
 var fs = require('fs');
 var path = require('path');
 var jobs = require('../../lib/jobs');
@@ -14,26 +16,41 @@ function getResolvedPath(pathToResolve, ext) {
 
 var detectMessage = function (req, res) {
   var resp = { 'status': 'fail', 'err': '', 'resp': `` };
-  
+
   var objectPath = req.data.objectPath || ``;
   var sceneImg = req.data.sceneImg || ``;
   var phone = req.data.phone || ``;
 
   if (objectPath && sceneImg && phone) {
-    objectPath = getResolvedPath(objectPath, `glat`);
-    sceneImg = getResolvedPath(sceneImg, `glat`);
-    var outputFileName = fileHandler.getName(phone);
-    console.log(`.${algoPath} ${objectPath} ${sceneImg} ${getResolvedPath(outputFileName, `jpg`)}`);
-    exec(`${algoPath} ${objectPath} ${sceneImg} ${getResolvedPath(outputFileName, `jpg`)}`, function (err, stdout, stderr) {
+    var asyncTasks = {};
+    asyncTasks[objectPath] = async.apply(fs.readFile, getResolvedPath(objectPath, `glat`));
+    asyncTasks[sceneImg] = async.apply(fs.readFile, getResolvedPath(sceneImg, `glat`));
+    async.parallel(asyncTasks, function (err, result) {
       if (err) {
         console.log(`[ERROR group/detectMessage] error: ${stderr}`);
         resp.err = err;
         res.json(resp);
       } else {
-        resp.status = `success`;
-        resp.resp = outputFileName;
-        fs.renameSync(getResolvedPath(outputFileName, `jpg`), getResolvedPath(outputFileName, `glat`));
-        res.json(resp);
+        let retObj = {};
+        Object.keys(asyncTasks).map((dictKey) => {
+          let tempFile = base64.decode(asyncTasks[dictKey]);
+          retObj[dictKey] = getResolvedPath(dictKey + `temp`, `glat`);
+          fs.writeFileSync(retObj[dictKey], tempFile);
+        });
+        var outputFileName = fileHandler.getName(phone);
+        console.log(`.${algoPath} ${retObj[objectPath]} ${retObj[sceneImg]} ${getResolvedPath(outputFileName, `jpg`)}`);
+        exec(`${algoPath} ${objectPath} ${sceneImg} ${getResolvedPath(outputFileName, `jpg`)}`, function (err, stdout, stderr) {
+          if (err) {
+            console.log(`[ERROR group/detectMessage] error: ${stderr}`);
+            resp.err = err;
+            res.json(resp);
+          } else {
+            resp.status = `success`;
+            resp.resp = outputFileName;
+            fs.renameSync(getResolvedPath(outputFileName, `jpg`), getResolvedPath(outputFileName, `glat`));
+            res.json(resp);
+          }
+        });
       }
     });
   } else {
@@ -60,5 +77,5 @@ if (require.main === module) {
       console.log(data);
     }
   }
-  detectMessage(req,res);
+  detectMessage(req, res);
 }
